fac29d0bda070bdbf280358e0fc8d7780231d6ab
Extra-codes <byaxyoxojayev@gmail.com>
2023-05-06 18:34:45 +0500

    Cool project

--- users/signals.py
+++ users/signals.py
@@ -23,14 +23,6 @@ def createProfile(sender, instance, created, **kwargs):
         subject = 'Welcome to DevSearch'
         message = 'We are glad you are here!'
 
-        send_mail(
-            subject,
-            message,
-            settings.EMAIL_HOST_USER,
-            [profile.email],
-            fail_silently=False,
-        )
-
 
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
